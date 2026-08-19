ee27b84d1be500cac018257a92c2f3bd77d1b554
Aadhesh <aadheshsridhar@gmail.com>
2026-08-19 23:23:23 +0530

    added git ignore

--- DSE/ex2_a.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-arr = np.array([[1, 2, 3], [4, 2, 6]])
-print(arr)
-
-print("Array is of type: ", type(arr))
-print("Array dimensions: ", arr.ndim)
-print("Array shape: ", arr.shape)
-print("Array size: ", arr.size)
-print("Array stores elements of type: ", arr.dtype)
